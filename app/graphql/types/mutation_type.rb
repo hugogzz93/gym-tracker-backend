74a36a9c939a6fc0ea2e0b4f28bb369377050bba
Hugo González <pinelo93@gmail.com>
2019-05-31 15:12:36 -0500

    Adds batch creation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
+    field :nutrient_intake, Mutations::NutrientIntakeOps, null: false do
+      argument :id, ID, required: false
+    end
 
     field :intake, Mutations::IntakeOps, null: false do
       argument :id, ID, required: false
@@ -7,6 +10,10 @@ module Types
 
     def intake(id: nil)
       id ? Intake.find(id) : Mutations::IntakeOps
+    end
+
+    def nutrient_intake(id: nil)
+      id ? NutrientIntake.find(id) : Mutations::NutrientIntakeOps
     end
   end
 end
--- lib/graphql_mutation.rb
+++ lib/graphql_mutation.rb
@@ -5,7 +5,7 @@ require 'active_support/concern'
 #     include GraphqlMutation
 #     is_mutation_of User
 #
-#     ...any averwrites here
+#     ...any overwrites here
 #   end
 #
 #   or inline
@@ -13,6 +13,41 @@ require 'active_support/concern'
 #   UserOps = GraphqlMutation.create_ops_for User
 module GraphqlMutation
   extend ActiveSupport::Concern
+
+  module MutationHelper
+
+    @create = proc do |_, input, _ctx, model|
+      model.create! input
+    end
+
+    @update = proc do |instance, input, _ctx, model|
+      instance.update! input
+      instance.reload
+    end
+
+    @destroy = proc do |instance|
+      begin
+        instance.destroy!
+        instance.id
+      rescue
+        nil
+      end
+    end
+
+    def self.create
+      @create
+    end
+
+    def self.update
+      @update
+    end
+
+    def self.destroy
+      @destroy
+    end
+  end
+
+
   def self.create_ops_for(model)
     Class.new(GraphQL::Schema::Object) do
       include GraphqlMutation
@@ -21,34 +56,67 @@ module GraphqlMutation
   end
 
   included do
-    def self.is_mutation_of(model)
 
+    def self.is_mutation_of(model)
       self.class_eval do
+        @model = model
+
         create = proc do |_, args, _ctx|
-          model.create! args.input.to_h
+          MutationHelper.create.call(_, args[:input].to_h, _ctx, model)
         end
 
-        update = proc do |instance, args, _ctx|
-          instance.update! args.input.to_h
-          instance.reload
+        update = proc do |_, args, _ctx|
+          MutationHelper.update.call(_, args[:input].to_h, _ctx, model)
         end
 
         destroy = proc do |instance|
-          instance.destroy!
+          MutationHelper.destroy.call(instance)
         end
 
-
         field :create, "Types::#{model.name}Type", null: false,
-              resolve: create do
+            resolve: create do
           argument :input, "Types::#{model.name}InputType", required: true
         end
 
         field :update, "Types::#{model.name}Type", null: false,
-              resolve: update do
+            resolve: update do
           argument :input, "Types::#{model.name}InputType", required: true
         end
 
-        field :destroy, GraphQL::Types::Boolean, null: false, resolve: destroy
+        field :destroy, [ GraphQL::Types::ID ], null: false, resolve: destroy
+      end
+    end
+
+    def self.is_batchable
+      model = @model
+      self.class_eval do
+        batch_create = proc do |_, args, _ctx|
+          args[:inputs].map do |input|
+            MutationHelper.create.call(_, input.to_h, _ctx, model)
+          end
+        end
+
+        # batch_update = proc do |_, args, _ctx|
+        #   array.map &MutationHelper.update
+        # end
+        #
+        # batch_destroy = proc do |_, args, _ctx|
+        # end
+
+        field :batch_create, [ "Types::#{model.name}Type" ], null: false,
+                resolve: batch_create do
+          argument :inputs, [ "Types::#{model.name}InputType" ], required: true
+        end
+
+        # field :update, [ "Types::#{model.name}Type" ], null: false,
+        #         resolve: batch_update do
+        #   argument :inputs, [ "Types::#{model.name}InputType" ], required: true
+        # end
+
+        # field :batch_destroy, [ GraphQL::Types::ID ], null: false,
+        #     resolve: batch_destroy do
+        #   argument :inputs, [ GraphQL::Types::ID ], required: true
+        # end
       end
     end
   end
